162ccceea399fa6fa16b92af644b2ca92fa4cf5b
TheGreyCord <104592742+TheGreyCord@users.noreply.github.com>
2022-04-29 13:05:49 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+settings = {
+    'token': '', 
+    'bot': 'Loovtöö Bot',
+    'id': '',
+    'Author': 'Dmitri Matetski',
+    'Version': '1.0.5',
+    'Lang': 'ENG',
+    'premmision': '1374863748342',
+    'avatar' : 'https://discord.com/assets/6f26ddd1bf59740c536d2274bb834a05.png',
+    'DB': 'db.db',
+}
+
+link = f"https://discordapp.com/oauth2/authorize?&client_id={settings['id']}&scope=bot&permissions={settings['premmision']}"
+
+API = {
+        'key': "",
+    }
